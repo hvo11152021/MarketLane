2b1b0c1a8fc1c273ba566a5300a915da9fb97c70
Hy Vo <94402902+hvo11152021@users.noreply.github.com>
2025-12-08 22:53:50 -0600

    Updated product dto and mapper

---
+++ backend/src/main/java/com/marketlane/backend/dto/ProductRequestDto.java
@@ -0,0 +1,22 @@
+package com.marketlane.backend.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class ProductRequestDto {
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    private byte[] imageData;
+}
---
+++ backend/src/main/java/com/marketlane/backend/mapper/ProductMapper.java
@@ -0,0 +1,41 @@
+package com.marketlane.backend.mapper;
+
+import com.marketlane.backend.dto.ProductRequestDto;
+import com.marketlane.backend.dto.ProductResponseDto;
+import com.marketlane.backend.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+    public Product toProduct(ProductRequestDto productRequestDto) {
+        Product product = new Product();
+        product.setName(productRequestDto.getName());
+        product.setDescription(productRequestDto.getDescription());
+        product.setBrand(productRequestDto.getBrand());
+        product.setPrice(productRequestDto.getPrice());
+        product.setCategory(productRequestDto.getCategory());
+        product.setReleaseDate(productRequestDto.getReleaseDate());
+        product.setProductAvailable(productRequestDto.isProductAvailable());
+        product.setStockQuantity(productRequestDto.getStockQuantity());
+        product.setImageName(productRequestDto.getImageName());
+        product.setImageType(productRequestDto.getImageType());
+        product.setImageData(productRequestDto.getImageData());
+        return product;
+    }
+    public ProductResponseDto toProductResponseDto(Product product) {
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setId(product.getId());
+        productResponseDto.setName(product.getName());
+        productResponseDto.setDescription(product.getDescription());
+        productResponseDto.setBrand(product.getBrand());
+        productResponseDto.setPrice(product.getPrice());
+        productResponseDto.setCategory(product.getCategory());
+        productResponseDto.setReleaseDate(product.getReleaseDate());
+        productResponseDto.setProductAvailable(product.isProductAvailable());
+        productResponseDto.setStockQuantity(product.getStockQuantity());
+        productResponseDto.setImageName(product.getImageName());
+        productResponseDto.setImageType(product.getImageType());
+        productResponseDto.setImageData(product.getImageData());
+        return productResponseDto;
+    }
+}
